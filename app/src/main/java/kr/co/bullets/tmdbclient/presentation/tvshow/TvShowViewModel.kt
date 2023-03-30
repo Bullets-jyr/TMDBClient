3aef8732c1b96f22dd7382e4bc6ba22c34f9f5a1
Bullets-jyr <jyr@bullets.co.kr>
2023-03-30 19:43:54 +0900

    137. Create The Presentation Layer : Part 2

---
+++ app/src/main/java/kr/co/bullets/tmdbclient/presentation/tvshow/TvShowViewModel.kt
@@ -0,0 +1,22 @@
+package kr.co.bullets.tmdbclient.presentation.tvshow
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import kr.co.bullets.tmdbclient.domain.usecase.GetTvShowsUseCase
+import kr.co.bullets.tmdbclient.domain.usecase.UpdateTvShowsUseCase
+
+class TvShowViewModel(
+    private val getTvShowsUseCase: GetTvShowsUseCase,
+    private val updateTvShowsUseCase: UpdateTvShowsUseCase
+) : ViewModel() {
+
+    fun getTvShows() = liveData {
+        val tvShowList = getTvShowsUseCase.execute()
+        emit(tvShowList)
+    }
+
+    fun updateTvShows() = liveData {
+        val tvShowList = updateTvShowsUseCase.execute()
+        emit(tvShowList)
+    }
+}
---
+++ app/src/main/java/kr/co/bullets/tmdbclient/presentation/tvshow/TvShowViewModelFactory.kt
@@ -0,0 +1,18 @@
+package kr.co.bullets.tmdbclient.presentation.tvshow
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import kr.co.bullets.tmdbclient.domain.usecase.GetMoviesUseCase
+import kr.co.bullets.tmdbclient.domain.usecase.GetTvShowsUseCase
+import kr.co.bullets.tmdbclient.domain.usecase.UpdateMoviesUseCase
+import kr.co.bullets.tmdbclient.domain.usecase.UpdateTvShowsUseCase
+
+class TvShowViewModelFactory(
+    private val getTvShowsUseCase: GetTvShowsUseCase,
+    private val updateTvShowsUseCase: UpdateTvShowsUseCase
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return TvShowViewModel(getTvShowsUseCase, updateTvShowsUseCase) as T
+    }
+}
