b2800b1e1ea388b9cde6ac3374a4136bde56b10d
Bullets-jyr <jyr@bullets.co.kr>
2023-04-02 17:44:32 +0900

    137. Create The Presentation Layer : Part 2 (Review)

--- app/src/main/java/kr/co/bullets/tmdbclient/presentation/movie/MovieViewModel.kt
+++ app/src/main/java/kr/co/bullets/tmdbclient/presentation/movie/MovieViewModel.kt
@@ -5,17 +5,26 @@ import androidx.lifecycle.liveData
 import kr.co.bullets.tmdbclient.domain.usecase.GetMoviesUseCase
 import kr.co.bullets.tmdbclient.domain.usecase.UpdateMoviesUseCase
 
+// From the view model we will only invoke those use case functions.
 class MovieViewModel(
     private val getMoviesUseCase: GetMoviesUseCase,
     private val updateMoviesUseCase: UpdateMoviesUseCase
 ) : ViewModel() {
 
+    // This function will invoke execute function of the getMoviesUseCase and emit received list as live data.
+    // Since we are using coroutines we will use a live data block here.
+    // We are not explicitly provide any dispatcher here. So coroutine will use the Main thread.
+    // Since we have used IO thread in the data sources, calling them from the UI thread is the best practice.
     fun getMovies() = liveData {
         val movieList = getMoviesUseCase.execute()
+        // the movieList as live data.
         emit(movieList)
     }
 
     fun updateMovies() = liveData {
+        // Here we will clear all the data in the movie table of the database, Download new data from the tmdb api and save them to database.
+        // We have already implemented those functions in the data layer.
+        // All we need to do is call to the execute function of the updateMoviesUseCase.
         val movieList = updateMoviesUseCase.execute()
         emit(movieList)
     }
--- app/src/main/java/kr/co/bullets/tmdbclient/presentation/movie/MovieViewModelFactory.kt
+++ app/src/main/java/kr/co/bullets/tmdbclient/presentation/movie/MovieViewModelFactory.kt
@@ -5,11 +5,17 @@ import androidx.lifecycle.ViewModelProvider
 import kr.co.bullets.tmdbclient.domain.usecase.GetMoviesUseCase
 import kr.co.bullets.tmdbclient.domain.usecase.UpdateMoviesUseCase
 
+// This view model class has constructor parameters.
+// Therefore, we need to create a view model factory for this view model.
+// This class should also have instances of GetMoviesUseCase and UpdateMoviesUseCase classes as constructor parameters.
+// This class should extend ViewModelProvider.Factory class.
 class MovieViewModelFactory(
     private val getMoviesUseCase: GetMoviesUseCase,
     private val updateMoviesUseCase: UpdateMoviesUseCase
 ) : ViewModelProvider.Factory {
 
+    // Let’s override the create function.
+    // Here we need to construct a MovieViewModel passing use case instances as artugments and return as T.
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return MovieViewModel(getMoviesUseCase, updateMoviesUseCase) as T
     }
